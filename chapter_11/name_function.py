1c48dc6a9a570142eb5fbbcef6aa2ddc9310e88f
Raihan9797 <raihanmuhd97@gmail.com>
2020-06-04 21:31:17 +0800

    chapter 11: failing test cases, resolving them and adding extra test cases

--- chapter_11/name_function.py
+++ chapter_11/name_function.py
@@ -1,7 +1,26 @@
 ### Testing a Function
 
-def get_formatted_name(first, last):
+
+## passing test
+# def get_formatted_name(first, last):
+    # """generate a neatly formatted full name"""
+    # fullname = first + ' ' + last
+    # return fullname.title()
+
+
+## failing test
+# def get_formatted_name(first, mid, last):
+    # """generate a neatly formatted full name"""
+    # fullname = first + ' ' + mid + ' ' + last
+    # return fullname.title()
+
+
+## resolved failed test
+def get_formatted_name(first, last, mid = ''):
     """generate a neatly formatted full name"""
-    fullname = first + ' ' + last
+    if mid:
+        fullname = first + ' ' + mid + ' ' + last
+    else:
+        fullname = first + ' ' + last
+
     return fullname.title()
-    
--- chapter_11/test_name_function.py
+++ chapter_11/test_name_function.py
@@ -10,6 +10,12 @@ class NamesTestCase(unittest.TestCase):
         formatted_name = get_formatted_name('janis', 'joplin')
         self.assertEqual(formatted_name, 'Janis Joplin')
 
+### Adding new test
+    def test_first_last_mid_name(self):
+        """Do names like 'Wolfgang Amadeus Mozart' work?"""
+        function_output = get_formatted_name('wolfgang', 'mozart', 'amadeus')
+        self.assertEqual(function_output, 'Wolfgang Amadeus Mozart')
+
 unittest.main()
 
 ### ABOUT UNITTEST ###
@@ -29,6 +35,8 @@ then run the code again
 '''
 
 '''
+THERE ARE REFERENCING PROBLEMS WHEN YOU TRY TO RUN IT THIS WAY:
+VSCODE WORK AREA IS ONE ISSUE AND THE TEST_FILES WHICH IMPORTS CHAPTER_11 MEAN THAT IF YOU GO IN CHAPTER_11 FOLDER, THEY WILL HAVE AN ERROR BECAUSE 'CHAPTER_11' MODULE IS NOT AVAILABLE....
 additionally, we can also run it in CMD by typing
 > python -m unittest
 However, you can't seem to do
@@ -40,3 +48,30 @@ However, you can't seem to do
 ### A Failing Test
 ## we will modify get_formatted_name() to accept middle names which will break the function if we just have 'Janis' and 'Joplin'
 
+'''
+>>> unittest.main()
+E    
+======================================================================
+ERROR: test_first_last_name (__main__.NamesTestCase)
+Do names like 'Janis Joplin' work?
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File "<stdin>", line 5, in test_first_last_name
+TypeError: get_formatted_name() missing 1 required positional argument: 'last'
+
+----------------------------------------------------------------------
+Ran 1 test in 0.001s
+
+FAILED (errors=1)
+'''
+# 1 E ==> 1 Error
+
+
+### Responding to a Failed Test
+'''
+DO NOT CHANGE THE TEST! IT'S LIKE CHANGING ANSWERS WHEN YOU ANSWERED WRONGLY
+
+we resolve this by making middle names optional
+
+MAKE SURE YOU ADD THE NEW TEST CASE FUNCTION IN THE UNITTEST CLASS!!
+'''
